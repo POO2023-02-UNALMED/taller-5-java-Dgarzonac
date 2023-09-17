04abae0929f93859e03019c35dbf69fd8f45b01d
Dgarzonac <123409139+Dgarzonac@users.noreply.github.com>
2023-09-17 11:41:26 -0500

    Update Animal.java

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -24,11 +24,7 @@ public class Animal {
 	}
 	
 	public static String totalPorTipo(){
-		return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" +
-			   "Aves: " + Ave.cantidadAves() + "\n" +
-			   "Reptiles: " + Reptil.cantidadReptiles() + "\n" +
-			   "Peces: " + Pez.cantidadPeces() + "\n" +
-			   "Anfibios: " + Anfibio.cantidadAnfibios();
+		return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" + "Aves: " + Ave.cantidadAves() + "\n" + "Reptiles: " + Reptil.cantidadReptiles() + "\n" + "Peces: " + Pez.cantidadPeces() + "\n" + "Anfibios: " + Anfibio.cantidadAnfibios();
 
 	}
 	
