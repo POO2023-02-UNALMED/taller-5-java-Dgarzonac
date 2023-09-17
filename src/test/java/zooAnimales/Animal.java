5f33463bff90fc4410c8dd2c18f03a7480797d03
Dgarzonac <123409139+Dgarzonac@users.noreply.github.com>
2023-09-17 11:43:27 -0500

    Update Animal.java

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -23,10 +23,7 @@ public class Animal {
 		return "desplazarse";
 	}
 	
-	public static String totalPorTipo(){
-		return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" + "Aves: " + Ave.cantidadAves() + "\n" + "Reptiles: " + Reptil.cantidadReptiles() + "\n" + "Peces: " + Pez.cantidadPeces() + "\n" + "Anfibios: " + Anfibio.cantidadAnfibios();
-
-	}
+	public static String totalPorTipo(){return "Mamiferos: "+Mamifero.cantidadMamiferos()+"\n" + "Aves: "+Ave.cantidadAves()+"\n" + "Reptiles: "+Reptil.cantidadReptiles()+"\n" + "Peces: "+Pez.cantidadPeces()+"\n" + "Anfibios: "+Anfibio.cantidadAnfibios();}
 	
 	public String toString (){
 		if (zona!=null){
